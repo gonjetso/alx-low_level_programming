11cc74d72ee94e22a1c66062b8162de9adfd0ade
gonjetso <gonjetsoc@gmail.com>
2022-09-08 07:38:16 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - this is where the program starts
+ *
+ * Description: printing statements according to Task Eight
+ *
+ * Return: ending the program
+ */
+
+int main(void)
+{
+	char Myst[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, Myst);
+	return (1);
+}
