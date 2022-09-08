d3e674cbcdba381f62adc151387bac9ec09953f4
gonjetso <gonjetsoc@gmail.com>
2022-09-08 06:28:32 -0700

    Task 4 fixes after Betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-int main()
+/** 
+ * Main: this is where the program starts
+ * and is found in all C programs
+ *
+ * Description: printing using puts is easier than printf
+ * this is a better description
+ *
+ * Return: ending the program
+ */
+
+int main(void)
 {
 	char Mystring[] = "\"Programming is like building a multilingual puzzle";
 		puts(Mystring);
-	return 0;
+	return (0);
 }
