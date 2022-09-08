28da86d6487401f9d10d9a4a2dcbd3f8073a0071
gonjetso <gonjetsoc@gmail.com>
2022-09-08 06:51:16 -0700

    Task printing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - this is where the program starts
+ *
+ * Description: printing using printf
+ *
+ * Return: ending the program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
