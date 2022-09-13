6970f3d7ced55fa820059b33c2fb60a47f35febf
gonjetso <gonjetsoc@gmail.com>
2022-09-13 01:29:24 -0700

    Task 0 for new project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Description: print putchar
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
