de39cc7cbd6c8464cfea084a87a0e47d00a7a78c
gonjetso <gonjetsoc@gmail.com>
2022-09-13 05:50:26 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+ * Return: Always zero
+ */
+void print_alphabet_x10(void)
+{
+	int alphacount;
+
+	while (alphacount < 10)
+	{
+		char alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		alphacount++;
+		_putchar('\n');
+	}
+}
