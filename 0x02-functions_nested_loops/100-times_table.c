3b7c14eb732689a0e4fb306730268bd5f45be72f
gonjetso <gonjetsoc@gmail.com>
2022-09-14 14:18:41 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,14 +1,52 @@
 #include "main.h"
 
 /**
- * print_last_digit - last digit
+ * print_times_table - print times table
  *
  * @n:n is int
  *
- * Return: lastdig
+ * Return: lastdigit
  */
-int print_last_digit(int n)
+void print_times_table(int n)
 {
+	int x, y, z;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (y == 0)
+				{
+					_putchar(z + '0');
+				}
+				else if (z < 10 && y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('z + '0');
+				}
+				else if (z >= 10 && z < 100)
+				{
+					_putchar(', ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z >= 100)
+				{
+					_putchar(', ');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+			}
 	int lastdig;
 
 	if (n < 0)
