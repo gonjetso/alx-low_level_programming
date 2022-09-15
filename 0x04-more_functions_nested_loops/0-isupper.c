69ef123fbc5f702596c63770f533e73d6591ae8b
gonjetso <gonjetsoc@gmail.com>
2022-09-15 07:04:18 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/**
+ * _isupper - check uppercase
+ * @c: input
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return(0);
+	}
+	else
+	{
+		return (1);
+	}
+}
