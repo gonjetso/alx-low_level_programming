c3f22104707ad81e09959eb3e95f155a4b96db63
gonjetso <gonjetsoc@gmail.com>
2022-09-16 14:35:43 -0700

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+
+/**
+ * main - main
+ * Description: prime factors
+ * Return: zero
+ */
+
+int main(void)
+{
+	unsigned long int n, x, factor;
+
+	n = 612852475143;
+	x = 2;
+	while (n != 0)
+	{
+		if (n % x != 0)
+		{
+			x++;
+		}
+		else
+		{
+			factor = n;
+			n = n / x;
+			if (n == 1)
+			{
+				printf("%lu\n", factor);
+				break;
+			}
+		}
+	}
+	return (0);
+}
