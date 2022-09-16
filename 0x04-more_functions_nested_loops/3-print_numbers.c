bc1dc06e03c0ceed2f307734824aa9072d0bf9e7
gonjetso <gonjetsoc@gmail.com>
2022-09-16 13:46:49 -0700

    More fixes for task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,9 @@
 
 /**
  * print_numbers - print numbers from 0 to 9
+ *
+ * Description: print numbers
+ *
  * Return: void
  */
 
@@ -9,9 +12,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 
 	_putchar('\n');
