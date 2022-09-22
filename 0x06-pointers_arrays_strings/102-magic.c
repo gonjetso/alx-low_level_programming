8618d3532c5e6f217afada7f8d6c4c6f939758cd
gonjetso <gonjetsoc@gmail.com>
2022-09-22 00:15:23 -0700

    Fixed Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,6 +8,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+	p[5] = 98;
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -18,7 +19,6 @@ int main(void)
 	 */
 	;
 	/* ...so that this prints 98\n */
-	p[5] = 98;
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
