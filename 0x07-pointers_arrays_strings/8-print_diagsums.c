23ee587f0fc62e8f20bb8b38d07df36f36426551
gonjetso <gonjetsoc@gmail.com>
2022-09-26 19:18:00 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - function
+ * @a: a
+ * @size: size
+ * Return: zero
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, k;
+	j = 0;
+	k = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+			j += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			k += *(a + i);
+	}
+	printf("%d, %d\n", j, k);
+}
