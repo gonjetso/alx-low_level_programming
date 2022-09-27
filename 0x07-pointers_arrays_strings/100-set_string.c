953e2082efe2d1cd4d0b7bfd38fea373aa51caa2
gonjetso <gonjetsoc@gmail.com>
2022-09-26 19:46:10 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function
+ * @s: s
+ * @to: to
+ * Return: zero
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 char *_strcat(char *dest, char *src);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
