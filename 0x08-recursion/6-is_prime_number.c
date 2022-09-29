bc29d8478508f21b263ac45cf1388fa220bbf1ba
gonjetso <gonjetsoc@gmail.com>
2022-09-28 19:57:02 -0700

    Task 4

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - function
+ * @n: n
+ * Return: zero
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
