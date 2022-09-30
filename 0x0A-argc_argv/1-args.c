6229df97d597bc787e2473b5e46ee8cbbbebef5d
gonjetso <gonjetsoc@gmail.com>
2022-09-30 12:05:55 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argc
+ * @argv: argv
+ * Description: desc
+ * Return: return
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
