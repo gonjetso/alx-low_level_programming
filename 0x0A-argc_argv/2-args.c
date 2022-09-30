3ae01ed613fd015d8bee2579bd069ed1627fe5d5
gonjetso <gonjetsoc@gmail.com>
2022-09-30 12:15:52 -0700

    task 2 fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,17 +10,10 @@
 int main(int argc, char *argv[])
 {
 	int count;
-
-	if (argc > 1)
+	
+	for (count = 0; count < argc; count++)
 	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("%s\n", argv[count]);
-		}
-	}
-	else
-	{
-		printf("The command had no other arguments.\n");
+		printf("%s\n", argv[count]);
 	}
 	return (0);
 }
