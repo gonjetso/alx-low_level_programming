46c9939d4b77393b002746f5167e91c4dae19551
gonjetso <gonjetsoc@gmail.com>
2022-09-30 11:58:31 -0700

    Arguments Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argc
+ * @argv: argv
+ * Description: desc
+ * Return: return
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
