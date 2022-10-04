970e9ca3b6662f7fca7fa9f49a92ff7e1d9d99d9
gonjetso <gonjetsoc@gmail.com>
2022-10-03 23:38:17 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,38 +1,51 @@
 #include <stdlib.h>
 
 /**
- * *_strdup - function
- * @str: str
+ * *str_concat - function
+ * @s1: s1
+ * @s2: s2
  * Return: zero
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-	char *x;
+	int i, j, x, y;
 
-	if (str == NULL)
+	x = 0;
+	y = 0;
+	char *s;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while (s1[x] != '\0')
+		x++;
+	while (s2[y] != '\0')
+		y++;
+
+	s = malloc((x * sizeof(char)) + ((y + 1) * sizeof(char)));
+
+	if (s == NULL)
 	{
 		return (NULL);
 	}
 
-	for (; str[j]  != '\0'; j++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		;
+		s[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		s[i] = s2[j];
+		i++;
 	}
 
-	x = malloc(j * sizeof(*str) + 1);
+	s[i] = '\0';
 
-	if (x == 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		for (; i < j; i++)
-		{
-			x[i] = str[i];
-		}
-	}
-	return (x);
+	return (s);
 }
